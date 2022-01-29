25be7b1f5efb362ae2f891da8a4c2eece892f456
sbrinton <YendisFish@YendisFish.YendisFish>
2022-01-29 11:17:08 -0700

    Added custom naming

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,62 @@ fn main() {
 
     fs::create_dir_all(&dir);
 
+    println!("vv Input a custom name for the solution. (Enter NULL for none) vv");
+
+    let mut univ = String::new();
+    std::io::stdin().read_line(&mut univ).expect("Not a valid input!");
+
+    match &univ.trim() {
+        &"NULL" => NoName(dir, univ),
+        _ => CustomName(dir, univ)
+    }
+}
+
+fn CustomName(dir: String, univ: String) {
+    std::process::Command::new("dotnet")
+    .arg("new")
+    .arg("sln")
+    .arg("-o")
+    .arg(&dir.trim())
+    .arg("--name")
+    .arg(&univ.trim())
+    .spawn().expect("couldn't execute");
+
+    std::thread::sleep(std::time::Duration::new(3, 3));
+
+    println!("vv Would you like to create a project for this solution? [y/n] vv");
+
+    let mut yorn = String::new();
+    std::io::stdin().read_line(&mut yorn).expect("Not a valid input!");
+
+    if &yorn.trim() == &String::from("y") {
+        
+        println!("vv What type of project do you want it to be? vv");
+        
+        let mut projtype = String::new();
+        std::io::stdin().read_line(&mut projtype).expect("Not a valid input!");
+
+        std::process::Command::new("dotnet")
+            .arg("new")
+            .arg(&projtype.trim())
+            .arg("-o")
+            .arg(&dir.trim())
+            .arg("--name")
+            .arg(&univ.trim())
+            .spawn().expect("couldn't execute");
+
+        std::thread::sleep(std::time::Duration::new(3, 3));
+
+        std::process::Command::new("dotnet")
+            .arg("sln")
+            .arg(&dir.trim())
+            .arg("add")
+            .arg(&dir.trim())
+            .spawn().expect("couldn't execute");
+    }
+}
+
+fn NoName(dir: String, univ: String) {
     std::process::Command::new("dotnet")
         .arg("new")
         .arg("sln")
@@ -16,7 +72,7 @@ fn main() {
         .spawn().expect("couldn't execute");
 
     std::thread::sleep(std::time::Duration::new(3, 3));
-    
+
     println!("vv Would you like to create a project for this solution? [y/n] vv");
 
     let mut yorn = String::new();
